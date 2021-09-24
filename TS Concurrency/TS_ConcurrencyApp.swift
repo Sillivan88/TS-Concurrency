24228cc132c46d089d332693a6bd6dc170d0e046
Thomas Sillmann <contact@thomassillmann.de>
2021-09-24 08:22:11 +0200

    Initial Commit

---
+++ TS Concurrency/TS_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TS_ConcurrencyApp.swift
+//  TS Concurrency
+//
+//  Created by Thomas Sillmann on 24.09.21.
+//
+
+import SwiftUI
+
+@main
+struct TS_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
